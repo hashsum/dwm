967e36480bd448e9d75fe72784144d2a18301fa2
arch <kogayv@gmail.com>
2020-04-09 00:10:08 +0600

    Delete unused files

--- dwm.c
+++ dwm.c
@@ -1087,12 +1087,12 @@ loadxrdb()
       xrdb = XrmGetStringDatabase(resm);
 
       if (xrdb != NULL) {
-        XRDB_LOAD_COLOR("dwm.color0", normbordercolor);
+        XRDB_LOAD_COLOR("dwm.color8", normbordercolor);
         XRDB_LOAD_COLOR("dwm.color0", normbgcolor);
-        XRDB_LOAD_COLOR("dwm.color6", normfgcolor);
-        XRDB_LOAD_COLOR("dwm.color8", selbordercolor);
-        XRDB_LOAD_COLOR("dwm.color14", selbgcolor);
-        XRDB_LOAD_COLOR("dwm.color0", selfgcolor);
+        XRDB_LOAD_COLOR("dwm.color7", normfgcolor);
+        XRDB_LOAD_COLOR("dwm.color7", selbordercolor);
+        XRDB_LOAD_COLOR("dwm.color2", selbgcolor);
+        XRDB_LOAD_COLOR("dwm.color7", selfgcolor);
       }
     }
   }
