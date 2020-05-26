94cbc743e4351b7ebad9492ae1efa6a55feefcf2
arch <kogayv@gmail.com>
2020-05-26 21:24:40 +0600

    Apply cfacts patch

--- config.h
+++ config.h
@@ -81,6 +81,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_bracketleft,     incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,               setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,               setmfact,       {.f = +0.05} },
+    { MODKEY|ShiftMask,             XK_h,               setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_l,               setcfact,       {.f = +0.25} },
 	{ MODKEY,                       XK_space,           zoom,           {0} },
 	{ MODKEY,                       XK_Tab,             view,           {0} },
 	{ MODKEY,                       XK_q,               killclient,     {0} },
